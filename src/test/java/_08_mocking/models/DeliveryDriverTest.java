983554d471f860a4adcd06c8941d2f884c5cb9bf
nolanbooth <nb@nb.org>
2025-04-12 16:34:05 -0700

    no

--- src/test/java/_08_mocking/models/DeliveryDriverTest.java
+++ src/test/java/_08_mocking/models/DeliveryDriverTest.java
@@ -13,33 +13,62 @@ class DeliveryDriverTest {
 
     DeliveryDriver deliveryDriver;
 
+    @Mock
+    CellPhone cellphone;
+    @Mock
+    Car car;
     @BeforeEach
     void setUp() {
-
+    	MockitoAnnotations.openMocks(this);
+    	
+    	deliveryDriver = new DeliveryDriver("John", car, cellphone);
     }
 
     @Test
     void itShouldWasteTime() {
         //given
+    	boolean expectedwastetime = true;
+    	
+    	when(deliveryDriver.wasteTime()).thenReturn(true);
+    	
+    	boolean actualwastetime = deliveryDriver.wasteTime();
     	
         //when
     	
         //then
+    	
+    	assertEquals(expectedwastetime, actualwastetime);
     }
 
     @Test
     void itShouldRefuel() {
         //given
-
+    	boolean expectedRefuel = true;
+    	
+    	int octaneLevel = 99;
         //when
-
+    	when(deliveryDriver.refuel(octaneLevel)).thenReturn(true);
+    	
+    	boolean actualRefuel = deliveryDriver.refuel(octaneLevel);
+    	
         //then
+    	
+    	assertEquals(expectedRefuel, actualRefuel);
     }
 
     @Test
     void itShouldContactCustomer() {
         //given
-
+    	boolean expectedContact = true;
+    	
+    	String phoneNumber = "911";
+    	
+    	when(deliveryDriver.contactCustomer(phoneNumber)).thenReturn(true);
+    	
+    	boolean actualContact = deliveryDriver.contactCustomer(phoneNumber);
+    	
+    	assertEquals(expectedContact, actualContact);
+    	
         //when
 
         //then
